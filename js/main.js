03610ae7e6dc1fec87d738a2a7bd230f2128bfcd
M-Osama96 <mohamedosama3466@gmail.com>
2025-09-07 11:54:18 +0300

    init commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+Fancybox.bind("[data-fancybox]");
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
+
+let count = 1; // يبدأ من 1
+const value = document.getElementById("value");
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+
+plus.addEventListener("click", () => {
+  count++;
+  value.textContent = count;
+});
+
+minus.addEventListener("click", () => {
+  if (count > 1) {
+    // علشان ميقلش عن 1
+    count--;
+    value.textContent = count;
+  }
+});
